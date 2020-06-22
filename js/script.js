abff81e3c46f73a170c4dd6c69abd83d20e61aac
Aidos <aidos.abilakimov99@mail.ru>
2020-06-22 14:43:05 +0600

    Made a Block Service and connected slider

--- js/script.js
+++ js/script.js
@@ -6,4 +6,42 @@ $(function (){
         $('nav ul').slideUp();
         }
     });
+
+
+    $('.service-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+            breakpoint: 600,
+            settings: {
+                dots: false,
+                centerMode: true,
+                centerPadding: '50px',
+            }
+        },
+        {
+            breakpoint: 400,
+            settings: {
+                dots: false,
+                centerMode: true,
+                centerPadding: '20px'
+            }
+        }
+    ]
+    });
+
+
+
+
+
+
+
+
+
+
 });
